2a55f8fe698646321877e4ff93c0346c0e979f9b
Rabeet Tanveer <153253449+Rabeet-Tanveer@users.noreply.github.com>
2024-06-27 01:44:48 -0700

    Update MailSent.py

--- MAIN/MailSent.py
+++ MAIN/MailSent.py
@@ -6,13 +6,13 @@ def SendMail(recipient, message):
 	# start TLS for security
 	s.starttls()
 	# Authentication
-	s.login("thetrendz360@gmail.com", "ecjg hojv xonn jzra")
+	s.login("sample1@gmail.com", "password_here")
 	# sending the mail
-	s.sendmail("thetrendz360@gmail.com", recipient, message)
+	s.sendmail("sample1@gmail.com", recipient, message)
 	# terminating the session
 	s.quit()
 
 
-recipient = "thetrendz360@gmail.com"
+recipient = "sample1@gmail.com"
 message = "Your child might be in stress!"
 
